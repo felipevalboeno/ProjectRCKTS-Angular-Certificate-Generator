963590b2c25ac135a20bc7856cad9d1c7b0a4380
Felipe Valboeno <feliperocha14@hotmail.com>
2025-09-15 14:36:36 -0300

    Finishing navbar, creating primary and secondary button

--- src/app/_components/navbar/navbar.ts
+++ src/app/_components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
-export class Navbar {
+export class Navbar implements OnInit{
+    ngOnInit(): void {
+      this.mensagem();
+
+    }
+
+
+    mensagem(){
+      console.log("Meu componente navbar inicializou");
+
+    }
 
 }
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
+import { PrimaryButton } from "./_components/primary-button/primary-button";
+import { SecondaryButton } from "./_components/secondary-button/secondary-button";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('gerador-certificado');
+  exibeNavbar: boolean = true;
 }
