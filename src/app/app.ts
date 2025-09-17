844a78bd990372d9afefca129e73dd22be30c5fb
Felipe Valboeno <feliperocha14@hotmail.com>
2025-09-17 19:56:53 -0300

    applying first routes configurations

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
-import { PrimaryButton } from "./_components/primary-button/primary-button";
-import { SecondaryButton } from "./_components/secondary-button/secondary-button";
-import { ItemCertificado } from "./_components/item-certificado/item-certificado";
+
+
+
 import { Baseui } from "./_components/baseui/baseui";
-import { Certificados } from "./pages/certificados/certificados";
-import { CertificadoForm } from "./pages/certificado-form/certificado-form";
-import { Certificado } from "./pages/certificado/certificado";
+
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificado, Baseui, Certificados, CertificadoForm, Certificado],
+  imports: [RouterOutlet, Navbar, Baseui],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
