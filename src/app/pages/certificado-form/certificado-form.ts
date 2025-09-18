ac817f62dbf5d155f6e6902d68aab03d0d5487e9
Felipe Valboeno <feliperocha14@hotmail.com>
2025-09-18 11:48:48 -0300

    applying some errors text rules

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { PrimaryButton } from "../../_components/primary-button/primary-button";
 import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
 import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [PrimaryButton, SecondaryButton, FormsModule],
+  imports: [PrimaryButton, SecondaryButton, FormsModule, CommonModule],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css'
 })
@@ -13,4 +14,9 @@ export class CertificadoForm {
   nome: string = ''
   atividade: string = ''
   atividades: string[] = ['Angular', 'React']
+
+
+  campoIvalido(control: NgModel){
+    return control.invalid && control.touched
+  }
 }
